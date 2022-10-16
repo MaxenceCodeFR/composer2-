a08a2abef2c2e8ed293c6707bf421677e2e67837
MaxenceCodeFR <105151136+MaxenceCodeFR@users.noreply.github.com>
2022-10-16 14:49:33 +0200

    Add files via upload

---
+++ composer1/src/hello.php
@@ -0,0 +1,12 @@
+<?php
+
+class Hello
+{
+
+    public function talk()
+    {
+        $mesage = 'Hello World';
+        $message = 'salut c\'est cool';
+        return $message . $mesage;
+    }
+}
